9c64a09d730ef66c3cc38172e0335b1790c030b8
shambu2k <sidharthshambu00@gmail.com>
2021-12-23 22:26:29 +0530

    init: harmonyOS JS boilerplate

---
+++ entry/src/main/java/com/example/ultraprocacher_sample/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.ultraprocacher_sample;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
